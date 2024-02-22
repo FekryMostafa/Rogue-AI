60638ee7516041c93256a09ac109e33ced697436
FekryMostafa <mostafaf25@mail.wlu.edu>
2024-02-22 12:39:45 -0500

    Infection circle is centered

--- server.py
+++ server.py
@@ -18,11 +18,12 @@ class Server(Drawable):
 
     def update(self, seconds):
         if self.infected:
-            self.infection_radius += 0.0001
+            self.infection_radius += 0.1
     
     def draw(self, surface, overlay):
         super().draw(surface)
+        print()
         if self.infected:
             infection_color = (0, 0, 0, 0)
-            pygame.gfxdraw.filled_circle(overlay, int(self.position[0]), int(self.position[1]), int(self.infection_radius), infection_color)
+            pygame.gfxdraw.filled_circle(overlay, int(self.position[0] + 0.5 * self.getSize()[0]), int(self.position[1] + 0.5 * self.getSize()[1]), int(self.infection_radius), infection_color)
 
